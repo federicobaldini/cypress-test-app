2ab6983e702998736150bca6240d5a2753586d8b
Federico Baldini <federico.baldini7@gmail.com>
2025-02-26 15:06:47 +0100

    Update basics.cy.js

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -9,6 +9,6 @@ describe("task page", () => {
   it("should display the page title", () => {
     cy.visit("http://localhost:5173/");
     cy.get(".main-header").get("h1").should("have.length", 1);
-    cy.get(".main-header").get("img").contains("My Cypress Course Tasks");
+    cy.get(".main-header").get("h1").contains("My Cypress Course Tasks");
   });
 });
