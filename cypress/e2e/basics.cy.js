836297cd1f131d3eb8c5bd89115f11dd76315360
Federico Baldini <federico.baldini7@gmail.com>
2025-02-26 13:13:15 +0100

    Update project

---
+++ cypress/e2e/basics.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe("task page", () => {
+  it("should render the main image", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get(".main-header img");
+  });
+
+  it("should display the page title", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get(".main-header h1").should("have.length", 1);
+    cy.get(".main-header h1").contains("My Cypress Course Tasks");
+  });
+});
