1bcdbdcdc7f983018001a671fccc6469aac31d54
Federico Baldini <federico.baldini7@gmail.com>
2025-02-26 15:30:29 +0100

    Update tasks.cy.js

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -13,4 +13,15 @@ describe("tasks management", () => {
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
   });
+
+  it("should create a new task", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("New Task");
+    cy.get("#summary").type("Some description");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get(".task").get("h2").contains("New Task");
+    cy.get(".task").get("p").contains("Some description");
+  });
 });
